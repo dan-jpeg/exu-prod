20337cc2c7315215ba142fad33744e28e13ac7d8
dan crowley <dancr.wley@gmail.com>
2024-12-13 12:20:18 -0500

    attempting to standardize font size across mobile browsers v12

--- src/components/more-info/ExtraSection.jsx
+++ src/components/more-info/ExtraSection.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const ExtraSection = ({ textContent0, textContent1, textContent2 }) => {
     return (
         <div className="bg-white text-sm font-alte-haas z-30">
-            <div className="bg-neutral-50 space-y-6 h-[100vh] transform translate-y-[50vh]  flex flex-col  items-end pr-12 pt-20">
-                <div className=" w-[200px] md:w-[352px] opacity-0 h-[115px] text-black text-xs font-normal">{textContent0}</div>
-                <div className=" w-[200px] md:w-[352px] h-[115px] text-black text-xs font-normal">{textContent0}</div>
-                <div className=" w-[200px] md:w-[352px] h-[115px] text-black text-xs font-normal">{textContent1}</div>
-                <div className=" w-[200px] md:w-[352px] h-[115px] text-black text-xs font-normal mt-auto">{textContent2}</div>
+            <div className="bg-neutral-50 space-y-6 h-[100vh] transform translate-y-[50vh]  flex flex-col  items-end pl-12 md:pl-auto md:pr-12 pt-20">
+                <div className=" w-full md:w-[352px] opacity-0 md:h-[115px] text-black text-xs font-normal">{textContent0}</div>
+                <div className=" w-full md:w-[352px] md:h-[115px] text-black text-xs font-normal">{textContent0}</div>
+                <div className=" w-full md:w-[352px] md:h-[115px] text-black text-xs font-normal">{textContent1}</div>
+                <div className=" w-full md:w-[352px] md:h-[115px] text-black text-xs font-normal mt-auto">{textContent2}</div>
             </div>
         </div>
     );
